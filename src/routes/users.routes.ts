48570ff12ce1f4ee380ccac9f451c1464bb10b24
WesleyMaciel2510 <wesley.2510@hotmail.com>
2024-02-01 14:26:53 -0300

    users.routes.ts working

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,27 +1,26 @@
-import { Router } from "express";
+import express from "express";
 import users from "../mock/users";
 
-const userRouter = Router();
+const userRouter = express.Router();
 
-//===========================================================
 // FUNCTIONS
 function searchUserID(ID: number) {
-  return users.find((user) => user.ID == ID);
+  return users.find((user) => user.ID === ID);
 }
-//===========================================================
-userRouter.get("/api/users", (req, res) => {
+
+userRouter.get("/", (req, res) => {
   res.json(users);
 });
-//===========================================================
-userRouter.get("/api/users/:ID", (req, res) => {
+
+userRouter.get("/:ID", (req, res) => {
   const foundUser = searchUserID(parseInt(req.params.ID));
   res.json(foundUser);
 });
-//===========================================================
-userRouter.post("/api/users", (req, res) => {
+
+userRouter.post("/", (req, res) => {
   console.log("POST CALLED");
   users.push(req.body);
-  res.status(201).send("User registered succesfully!");
+  res.status(201).send("User registered successfully!");
 });
 
 export default userRouter;
--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,26 @@ import express from "express";
 import { createServer } from "http";
 import { testFirebase } from "./fireBaseTest";
 import users from "./mock/users";
-//import userRouter from "./routes/users.routes";
+import userRouter from "./routes/users.routes";
 import ip from "ip";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-//app.use("/api/users", userRouter);
+app.use("/api/users", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello from the server!");
 });
-//===========================================================
+app.listen(port, () => {
+  const ipAddresses = ip.address();
+  console.log(`Server listening on addresses: http://localhost:${port}`);
+  console.log(`IP === ${ipAddresses}`);
+});
+
+testFirebase();
+/* //===========================================================
 // FUNCTIONS
 function searchUserID(ID: number) {
   return users.find((user) => user.ID == ID);
@@ -33,12 +40,5 @@ app.post("/api/users", (req, res) => {
   console.log("POST CALLED");
   users.push(req.body);
   res.status(201).send("User registered succesfully!");
-});
+}); */
 //===========================================================
-app.listen(port, () => {
-  const ipAddresses = ip.address();
-  console.log(`Server listening on addresses: http://localhost:${port}`);
-  console.log(`IP === ${ipAddresses}`);
-});
-
-testFirebase();
